4cd1e10f63a0e3b5409f680b142c14e21943f751
Daniel <daniel.gomes.9@hotmail.com>
2022-11-25 15:13:59 -0300

    exercicios do course

---
+++ fundamentos/seção04-JavaScript/Dia01_primeiros_passos/scripts.js
@@ -0,0 +1,22 @@
+const myName = 'Daniel';
+
+const birthCity = 'Mogi das Cruzes';
+
+let birthYear = 1997;
+
+console.log(myName , birthCity , birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+
+const base = 5;
+const height = 8;
+
+const area = base * height;
+ console.log(area);
+
+ const perimeter = 5 + 5 + 8 + 8;
+
+ console.log(perimeter);
+ 

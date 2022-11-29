87e3499197a885ff7e072545442c73f5f0a920a7
Daniel <daniel.gomes.9@hotmail.com>
2022-11-29 18:53:43 -0300

    exercicio resolvido fatorial 10

---
+++ fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
@@ -0,0 +1,18 @@
+// let fatorial = 10;
+
+// let resultado = 0;
+
+// for (let index = 1; index < fatorial; index += 1) {
+//     resultado = index * fatorial;
+// }
+// console.log(resultado);
+
+// let soma = 10!;
+
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+  fatorial *= index;
+}
+
+console.log(fatorial);

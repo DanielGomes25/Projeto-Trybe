b8a608fcbe028c00dcff7aa19a1e44918d65995f
Daniel <daniel.gomes.9@hotmail.com>
2022-11-29 20:04:15 -0300

    exercio palavra invertida finalizado

--- fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
+++ fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
@@ -7,12 +7,21 @@
 // }
 // console.log(resultado);
 
-// let soma = 10!;
+// // let soma = 10!;
 
-let fatorial = 1;
+// let fatorial = 1;
 
-for (let index = 10; index > 0; index -= 1) {
-  fatorial *= index;
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+let word = 'tryber';
+let resultado = '';
+
+
+for (let index = 0; index < word.length; index += 1) {
+resultado += word[word.length - 1 - index];
 }
-
-console.log(fatorial);
+console.log(resultado)

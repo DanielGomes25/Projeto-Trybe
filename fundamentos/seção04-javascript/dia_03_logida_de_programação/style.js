0e8e8543bb9cca9a4142987b44d6f8c3a476d6db
Daniel <daniel.gomes.9@hotmail.com>
2022-11-30 09:14:07 -0300

    usando for para encontrar maior e menor palavra

--- fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
+++ fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
@@ -1,27 +1,46 @@
-// let fatorial = 10;
+// // let fatorial = 10;
 
-// let resultado = 0;
+// // let resultado = 0;
 
-// for (let index = 1; index < fatorial; index += 1) {
-//     resultado = index * fatorial;
+// // for (let index = 1; index < fatorial; index += 1) {
+// //     resultado = index * fatorial;
+// // }
+// // console.log(resultado);
+
+// // // let soma = 10!;
+
+// // let fatorial = 1;
+
+// // for (let index = 10; index > 0; index -= 1) {
+// //   fatorial *= index;
+// // }
+
+// // console.log(fatorial);
+
+// let word = 'tryber';
+// let resultado = '';
+
+
+// for (let index = 0; index < word.length; index += 1) {
+// resultado += word[word.length - 1 - index];
 // }
-// console.log(resultado);
+// console.log(resultado)
 
-// // let soma = 10!;
+let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-// let fatorial = 1;
+let biggestWord = array[0];
+let smallestWord = array[0];
 
-// for (let index = 10; index > 0; index -= 1) {
-//   fatorial *= index;
-// }
-
-// console.log(fatorial);
-
-let word = 'tryber';
-let resultado = '';
-
-
-for (let index = 0; index < word.length; index += 1) {
-resultado += word[word.length - 1 - index];
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length > biggestWord.length) {
+        biggestWord = array[index];
+    }
 }
-console.log(resultado)
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length < smallestWord.length){
+        smallestWord = array[index];
+    }
+}
+console.log(biggestWord);
+console.log(smallestWord);
+

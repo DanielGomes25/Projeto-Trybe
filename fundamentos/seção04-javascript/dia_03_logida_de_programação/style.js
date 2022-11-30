a7c942bf1a5afd096e8771544a95d4fd6b84611c
Daniel <daniel.gomes.9@hotmail.com>
2022-11-30 11:26:37 -0300

    exercio de encotrar numero par resolvido

--- fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
+++ fundamentos/seção04-javascript/dia_03_logida_de_programação/style.js
@@ -28,19 +28,43 @@
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let biggestWord = array[0];
-let smallestWord = array[0];
+// let biggestWord = array[0];
+// let smallestWord = array[0];
 
-for (let index = 0; index < array.length; index += 1){
-    if (array[index].length > biggestWord.length) {
-        biggestWord = array[index];
-    }
-}
-for (let index = 0; index < array.length; index += 1){
-    if (array[index].length < smallestWord.length){
-        smallestWord = array[index];
-    }
-}
-console.log(biggestWord);
-console.log(smallestWord);
+// for (let index = 0; index < array.length; index += 1){
+//     if (array[index].length > biggestWord.length) {
+//         biggestWord = array[index];
+//     }
+// }
+// for (let index = 0; index < array.length; index += 1){
+//     if (array[index].length < smallestWord.length){
+//         smallestWord = array[index];
+//     }
+// }
+// console.log(biggestWord);
+// console.log(smallestWord);
 
+// let biggestPrimeNumber = 0;
+
+// for (let index = 2; index <= 50; index += 1){
+//     if (index / 1 && index){
+//         biggestPrimeNumber = index;
+//     }
+// }
+// console.log(biggestPrimeNumber);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
